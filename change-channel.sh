cb446b074c27cfb7b03fc7074d2d23ea3e7898c4
Bart Leenheer <b.leenheer@student.utwente.nl>
2021-06-15 23:57:48 +0200

    Changed up a lot, fixed most bugs

--- change-channel.sh
+++ change-channel.sh
@@ -1,53 +1,71 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: change-channel.sh -c CHANNEL -b BANDWIDTH [-t TIMEOUT]"
+  echo "--Router: Usage: change-channel.sh -c CHANNEL -b BANDWIDTH [-t TIMEOUT]"
 }
 
 if [[ $# -lt 2 ]]; then
-    echo "Invalid number of arguments"
-    usage
-    exit
+  echo "--Router: Invalid number of arguments"
+  usage
+  exit
 fi
 
-TIMEOUT=60
+TIMEOUT=90
 
 channel=44
 bandwidth=20
 
-while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
-  -t )
-    shift;
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
+  case $1 in
+  -t)
+    shift
     TIMEOUT=$1
     ;;
-  -h )
+  -h)
     usage
     exit
     ;;
-  -c )
-    shift;
+  -c)
+    shift
     channel=$1
     ;;
-  -b )
-    shift;
+  -b)
+    shift
     bandwidth=$1
     ;;
-esac; shift; done
+  esac
+  shift
+done
 if [[ "$1" == '--' ]]; then shift; fi
 
-ip link set wlan0 down
-iw dev wlan0 set channel "$channel" "HT$bandwidth"
-ip link set wlan0 up
+echo "--Router: Changing to channel $channel and bandwidth $bandwidth"
 
-starttime=$(date +%s)
-while [[ -z $(iw dev wlan0 station dump) ]]; do
+(
+  echo "$channel + $bandwidth (timeout $TIMEOUT):" >>test.txt
+  echo "1" >>test.txt
+
+  ip link set wlan0 down
+  echo "2" >>test.txt
+  iw dev wlan0 set channel "$channel" "HT$bandwidth"
+  echo "3" >>test.txt
+  ip link set wlan0 up
+  echo "4" >>test.txt
+
+  starttime=$(date +%s)
+  while [[ -z $(iw dev wlan0 station dump) ]]; do
     if [[ $(date +%s) -gt $(($starttime + $TIMEOUT)) ]]; then
-        echo "Timeout reached, reverting back to channel 44 at 20MHz"
-        ip link set wlan0 down
-        iw dev wlan0 set channel "44" "HT20"
-        ip link set wlan0 up
-        exit
+      echo "Timed out -> going back to 44 20">>test.txt
+      ip link set wlan0 down
+      iw dev wlan0 set channel "44" "HT20"
+      ip link set wlan0 up
+      echo "Successfully went back to 44 20">>test.txt
+      exit
     fi
-    sleep 0.1
-done
-echo "Device successfully reconnected"
+    sleep 0.5
+  done
+  echo "Device successfully reconnected">>test.txt
+
+) &
+disown %1
+
+echo "--Router: when the user doesn't reconnect within $TIMEOUT s old settings will be reverted"
--- run.sh
+++ run.sh
@@ -2,113 +2,167 @@
 
 CHANNELS=(34 36 38 40 42 44 46 48 52 56 60 64 100 104 108 112 116 120 124 128 132 136 140 144 149 153 157 161 165)
 BANDWIDTH=(20 40- 40+)
+# CHANNELS=(112)
+# BANDWIDTH=(20)
 
 help() {
-    echo "Usage: ./run.sh [-i IP] [-u USER] [-l LOGFILE] [-r RESULTFILE] [-v] [--keyfile KEYFILE] [-t TIMEOUT]"
+  echo "Usage: ./run.sh [-i IP] [-u USER] [-l LOGFILE] [-r RESULTFILE] [-v] [--keyfile KEYFILE] [-t TIMEOUT] [--skip-search] [--skip-optimize]"
 }
 
 IP="192.168.1.3"
 USER="root"
 LOGFILE="log.txt"
 RESULTFILE="result.txt"
-VERBOSE=0
+VERBOSE=false
 KEYFILE=""
 APNAME=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2 | sed 's/yes://')
-TIMEOUT=60
+TIMEOUT=90
+SEARCH=true
+OPTIMIZE=true
 
-# Checks optional arguments 
-while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
-  -u )
-    shift;
+# Checks optional arguments
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
+  case $1 in
+  -u)
+    shift
     USER=$1
     ;;
-  -i )
-    shift;
+  -i)
+    shift
     IP=$1
     ;;
-  -h )
+  -h)
     help
     exit
     ;;
-  -v )
-    VERBOSE=1
+  -v)
+    VERBOSE=true
     ;;
-  -l )
-    shift;
+  -l)
+    shift
     LOGFILE=$1
     ;;
-  -r )
-    shift;
+  -r)
+    shift
     RESULTFILE=$1
     ;;
-  --keyfile ) 
-    shift;
+  --keyfile)
+    shift
     KEYFILE=$1
     ;;
-  -t )
-    shift;
+  -t)
+    shift
     TIMEOUT=$1
     ;;
-esac; shift; done
+  --skip-search)
+    SEARCH=false
+    ;;
+  --skip-optimize)
+    OPTIMIZE=false
+    ;;
+  esac
+  shift
+done
 if [[ "$1" == '--' ]]; then shift; fi
 
-# Sends ping commands
+get_current_settings() {
+  echo $(ssh -i "$KEYFILE" "$USER@$IP" "iw wlan0 info | grep channel | sed 's/channel //' | sed 's/(.*), width: //'")
+}
+
 send_packets() {
-    local interval=$1
-    local size=$(($2-8))
-    echo "$interval $size" >> "$LOGFILE"
-    local result=$(sudo ping -i "$interval" -c 100 -s "$size" "$IP" | sed -n '/--- .* ping statistics ---/,$p' | sed '1d' | sed 's/packets transmitted, //' | sed 's/received, //' | sed 's/packet loss, //' | sed 's/time //' | sed ':a;N;$!ba;s/\n/,/g' | sed 's%ms,rtt min/avg/max/mdev = % %' | sed 's/ ms//' | sed -r 's%/% %g')
-    echo "$result" >> "$LOGFILE"
-    echo "$result"
+  local interval=$1
+  local size=$(($2 - 8))
+  echo "$interval $size" >>"$LOGFILE"
+  local result=$(sudo ping -i "$interval" -c 100 -s "$size" "$IP" | sed -n '/--- .* ping statistics ---/,$p' | sed '1d' | sed 's/packets transmitted, //' | sed 's/received, //' | sed 's/packet loss, //' | sed 's/time //' | sed ':a;N;$!ba;s/\n/,/g' | sed 's%ms,rtt min/avg/max/mdev = % %' | sed 's/ ms//' | sed -r 's%/% %g')
+  echo "$result" >>"$LOGFILE"
+  echo "$result"
 }
 
 reconnect() {
-    local starttime=$(date +%s)
-    while [[ -n $(nmcli c up "$APNAME" | grep "Error") ]]; do
-        sleep 0.1
-    done
-    if [[ $(date +%s) -gt $(($starttime + $TIMEOUT)) ]]; then
-        echo "error"
-    fi
+  local starttime=$(date +%s)
+  while [[ -n $(nmcli c up "$APNAME" 2>&1 | grep "Error") || $(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2 | sed 's/yes://') -ne "$APNAME" ]]; do
+    sleep 0.1
+  done
+  if [[ $(date +%s) -gt $(($starttime + $TIMEOUT)) ]]; then
+    $VERBOSE && echo "Surpassed timeout ($TIMEOUT s), resetting to old settings" &>/dev/null
+  fi
+  echo "$(date +%s) - $starttime" | bc
+  sleep 0.5
+}
+
+change_settings() {
+  local channel=$1
+  local bandwidth=$2
+  $VERBOSE && echo "Ch:$channel bw:$bandwidth"
+  local return=$(ssh -i "$KEYFILE" "$USER@$IP" "/root/change-channel.sh -c \"$channel\" -b \"$bandwidth\" -t \"$TIMEOUT\"")
+  $VERBOSE && echo $return
+  sleep 5
 }
 
 check_speed() {
-    local channel=$1
-    local bandwidth=$2
-    echo "$channel $bandwidth" >> "$LOGFILE"
-    echo "Now channel+bandwidth: $channel+$bandwidth"
-    (
-        ssh -i "$KEYFILE" "$USER@$IP" "/root/change-channel.sh -c \"$channel\" -b \"$bandwidth\" -t \"$TIMEOUT\""
-    ) &
-    disown %1
-    sleep 1
-    echo "Trying to reconnect"
-    if [[ -z $(reconnect) ]]; then # Only when able to reconnect we try pinging
-        local health=0.0
-        for interval in 0.1 0.01 0.001; do
-            for size in 10000 30000 60000; do
-                echo "Now pinging $interval $size"
-                local result=$(send_packets $interval $size)
-                local rtt=$(echo $result | awk '{print$7}') # Obtain the average rtt from the results
-                local packetLoss=$(echo $result | awk '{print$3}' | sed "s/%//" | awk '{print "scale=2; " $0 " / 100"}' | bc) # Convert percentage into floating point
-                health=$(echo "scale=5; $health + ($rtt * (1 + $packetLoss))" | bc) # Calculate health with 4 decimals
-                echo "$health"
-            done
-        done
-        echo "$health $channel $bandwidth" >> "$RESULTFILE"
-    else
-        echo "Could not reconnect, trying different bandwidth + channel"
-    fi
+  local channel=$1
+  local bandwidth=$2
+  echo "$channel $bandwidth" >>"$LOGFILE"
+  echo "Now trying channel+bandwidth: $channel+$bandwidth"
+  change_settings $channel $bandwidth
+  $VERBOSE && echo "Trying to reconnect"
+  local timetaken=$(reconnect)
+  echo "$timetaken" >>"$LOGFILE"
+  $VERBOSE && echo "Time taken to reconnect: $timetaken s"
+  local result=$(get_current_settings)
+  if [[ "$(echo "$result" | awk '{print $1}')" -eq "$channel" && "$(echo "$result" | awk '{print $2}')" -eq "$(echo "$bandwidth" | sed 's/+//' | sed 's/-//')" ]]; then
+    local health=0.0
+    for interval in 0.1 0.01 0.001; do
+      for size in 10000 30000 60000; do
+        $VERBOSE &&echo "Now pinging $interval $size"
+        local result=$(send_packets $interval $size)
+        $VERBOSE && echo "Ping results (transmitted, received, packet loss, time (ms), rtt min/avg/max/mdev): $result"
+        local rtt=$(echo $result | awk '{print$7}')                                                                   # Obtain the average rtt from the results
+        local packetLoss=$(echo $result | awk '{print$3}' | sed "s/%//" | awk '{print "scale=2; " $0 " / 100"}' | bc) # Convert percentage into floating point
+        newhealth=$(echo "scale=5; $health + ($rtt * (1 + $packetLoss))" | bc)                                           # Calculate health with 4 decimals
+        $VERBOSE && echo "The new health score is: $newhealth, by calculating $health (old health) + ($rtt (round trip time) * (1 + $packetLoss (packetloss))) = $newhealth"
+        health=$newhealth
+      done
+    done
+    echo "$health $channel $bandwidth ($timetaken)" >>"$RESULTFILE"
+    echo "health: $health" >>"$LOGFILE"
+  else
+    $VERBOSE && echo "Channel and bandwidth do not match: local: $channel $bandwidth, router: $(echo "$result" | awk '{print $1}') $(echo "$result" | awk '{print $2}')"
+  fi
 
-    echo "" >> "$LOGFILE"
+  echo "" >>"$LOGFILE"
 }
 
-touch $LOGFILE
-touch $RESULTFILE
-rm $LOGFILE
-rm $RESULTFILE
-for channel in "${CHANNELS[@]}"; do
-    for bandwidth in "${BANDWIDTH[@]}"; do
-        check_speed $channel $bandwidth
+if $SEARCH; then
+  echo "Started searching optimal channel and bandwidth combination"
+  $VERBOSE && echo "Removing old logfile and resultfile"
+  touch $LOGFILE
+  touch $RESULTFILE
+  rm $LOGFILE
+  rm $RESULTFILE
+  for bandwidth in "${BANDWIDTH[@]}"; do
+    for channel in "${CHANNELS[@]}"; do
+      check_speed $channel $bandwidth
     done
-done
+  done
+  echo "Started searching optimal channel and bandwidth combination (results can be found in $RESULTFILE)"
+fi
+
+if $OPTIMIZE; then
+  echo "Started optimizing"
+  BEST=""
+  while read line; do
+    if [[ -z "$BEST" ]] || (( $(echo "$(echo "$line" | awk '{print $1}') < $(echo "$BEST" | awk '{print $1}')" | bc -l) )); then
+      $VERBOSE && echo "Changing best from $BEST to $line"
+      BEST="$line"
+    fi
+  done <"$RESULTFILE"
+  $VERBOSE && echo "The final best is $BEST"
+  channel=$(echo "$BEST" | awk '{print $2}')
+  bandwidth=$(echo "$BEST" | awk '{print $3}')
+  change_settings $channel $bandwidth
+  timetaken=$(reconnect)
+  $VERBOSE && current_settings=$(get_current_settings)
+  $VERBOSE && echo "Time taken to reconnect: $timetaken s, new settings: channel: $(echo $current_settings | awk '{print $1}'), bandwidth: $(echo $current_settings | awk '{print $2}')"
+  echo "Done optimizing"
+fi
