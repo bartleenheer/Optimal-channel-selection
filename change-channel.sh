e2c622dbd57b06e4c5a80f934f70fcc75a23148d
Bart Leenheer <b.leenheer@student.utwente.nl>
2021-06-16 11:16:35 +0200

    Removed debug echoes from change-channel.sh

--- change-channel.sh
+++ change-channel.sh
@@ -41,29 +41,21 @@ if [[ "$1" == '--' ]]; then shift; fi
 echo "--Router: Changing to channel $channel and bandwidth $bandwidth"
 
 (
-  echo "$channel + $bandwidth (timeout $TIMEOUT):" >>test.txt
-  echo "1" >>test.txt
 
   ip link set wlan0 down
-  echo "2" >>test.txt
   iw dev wlan0 set channel "$channel" "HT$bandwidth"
-  echo "3" >>test.txt
   ip link set wlan0 up
-  echo "4" >>test.txt
 
   starttime=$(date +%s)
   while [[ -z $(iw dev wlan0 station dump) ]]; do
     if [[ $(date +%s) -gt $(($starttime + $TIMEOUT)) ]]; then
-      echo "Timed out -> going back to 44 20">>test.txt
       ip link set wlan0 down
       iw dev wlan0 set channel "44" "HT20"
       ip link set wlan0 up
-      echo "Successfully went back to 44 20">>test.txt
       exit
     fi
     sleep 0.5
   done
-  echo "Device successfully reconnected">>test.txt
 
 ) &
 disown %1
